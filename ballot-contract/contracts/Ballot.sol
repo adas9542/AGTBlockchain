357ade8678ed22ef8a5709b64af1103ef1bce24f
Arnab <33712730+adas9542@users.noreply.github.com>
2019-11-09 14:47:23 -0500

    Add files via upload

--- ballot-contract/contracts/Ballot.sol
+++ ballot-contract/contracts/Ballot.sol
@@ -1,105 +1,47 @@
-pragma solidity ^0.5.2;
-contract BallotV4 {
+pragma solidity ^0.5.0;
+contract Ballot {
 
     struct Voter {                     
-        uint weight;
+        uint weight; //their vote
         bool voted;
-        uint8 vote;
-        uint8 votes;
-        bool paymentinfo;
-        bool purchasedToday;
+        uint8 vote; //who they voted for 
     }
     struct Proposal {                  
         uint voteCount;
     }
 
     address chairperson;
-    mapping(address => Voter) voters;  //voter addresses and names
-    Proposal[] proposals; //number of proposals used from length of array
+    mapping(address => Voter) voters;  
+    Proposal[] proposals;
+    uint public startTime;
     
-
-    enum Phase {Init,Regs, Vote, Done}   //user-defined types
-    Phase public state = Phase.Done; 
-    
-       //modifiers
-    modifier validPhase(Phase reqPhase) 
-     { require(state == reqPhase); 
-      _; 
-     } 
-    modifier onlyChair() 
-     {require(msg.sender == chairperson);
-      _;
-     }
-     modifier hasVotes()
-     {require(voters[msg.sender].votes >= 1);
-      _;
-     }
-     modifier hasMoney()
-     {require(voters[msg.sender].paymentinfo == true);
-     _;
-     }
-     
-    constructor (uint8 numProposals) public  {
+    constructor(uint8 numProposals) public  {
         chairperson = msg.sender;
         voters[chairperson].weight = 2; // weight 2 for testing purposes
         proposals.length = numProposals;
-        state = Phase.Regs;
     }
     
-     function changeState(Phase x) onlyChair public {
-        
-        require (x > state );
-      
-        state = x;
-     }
-    
-    function register(address voter) public validPhase(Phase.Regs) onlyChair {
-       
-        require (! voters[voter].voted);
-        
+    function register(address voter) public  {
         voters[voter].weight = 1;
-        voters[voter].votes = 10;
-        voters[voter].paymentinfo = true;
-        voters[voter].purchasedToday = false;
-        
-       // voters[voter].voted = false;
+        voters[voter].voted = false;
     }
 
    
-    function vote(uint8 toProposal) hasVotes() public{ //checks to see if the sender has voted or not
-      
+    function vote(uint8 toProposal) public  {
         Voter memory sender = voters[msg.sender];
-        
-        require (!sender.voted); 
-        require (toProposal < proposals.length); 
-        require (sender.votes >= 1);
-        
+        if (sender.voted || toProposal >= proposals.length) return; 
         sender.voted = true;
         sender.vote = toProposal;   
-        proposals[toProposal].voteCount += 1;
-        voters[msg.sender].votes -= 1;
+        proposals[toProposal].voteCount += sender.weight;
     }
 
-    function reqWinner() public validPhase(Phase.Done) view returns (uint8 winningProposal) { //checksforwinning
-       
+    function reqWinner() public view returns (uint8 _winningProposal) {
         uint256 winningVoteCount = 0;
         for (uint8 prop = 0; prop < proposals.length; prop++) 
             if (proposals[prop].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[prop].voteCount;
-                winningProposal = prop;
+                _winningProposal = prop;
             }
-       assert(winningVoteCount>=1); //check for state changes and overflow/underflow
-    }
-    
-    function checkVotes() public view returns (uint8 votesLeft) {
-        
-        votesLeft = voters[msg.sender].votes;
-    }
-    
-    function buyVotes() public hasMoney(){
-        require(voters[msg.sender].purchasedToday = false);
-        
-        voters[msg.sender].votes += 10;
-        voters[msg.sender].purchasedToday = true;
+       assert(winningVoteCount>=1);
     }
 }
